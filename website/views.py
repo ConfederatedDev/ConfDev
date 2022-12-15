0edba47bc8950eaff095990d87e33ef163fc5a31
Conf Dev <109419240+ConfederatedDev@users.noreply.github.com>
2022-12-15 10:38:53 -0500

    Added in remove BG

--- website/views.py
+++ website/views.py
@@ -1,10 +1,20 @@
-from flask import Blueprint, render_template, request, flash, jsonify, Response
+from flask import Blueprint, render_template, request, flash, jsonify, Response,current_app,url_for
 from flask_login import login_required, current_user
+from werkzeug.utils import secure_filename
 from . import db
 import json
+import os
 import smtplib
 from email.message import EmailMessage
+from PIL import Image
+import base64
+import io
+from rembg import remove
 
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 views = Blueprint('views', __name__)
 
@@ -17,6 +27,35 @@ def home():
 def projects():
 
     return render_template("projects.html", user=current_user)
+
+@views.route('/projects/removeBG',methods=['GET','POST'])
+def removeBG():
+    #print(current_app.config)
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return render_template("removeBG.html", user=current_user)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return render_template("removeBG.html", user=current_user)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            
+            file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+            im = Image.open(file)
+            output = remove(im)#.convert('RGB')
+            data = io.BytesIO()
+            output.save(data, "PNG")
+            encoded_img_data = base64.b64encode(data.getvalue())
+            #,linkBack=url_for('views.removeBG')
+            return render_template("removedBG.html", user=current_user, img_data=encoded_img_data.decode('utf-8'))
+    return render_template("removeBG.html", user=current_user)
+
+@views.route('/projects/removedBG',methods=['GET','POST'])
+def removedBG():
+
+    return render_template("removeBG.html", user=current_user)
 
 @views.route('/contact',methods=['GET','POST'])
 def contact():
